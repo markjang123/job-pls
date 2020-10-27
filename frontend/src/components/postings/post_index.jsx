09c1532c21f3ca055895cf46d85613e0dbf70cc5
Mac Mini <ryan@cygaq.com>
2020-10-27 14:40:19 -0700

    working on post show

--- frontend/src/components/postings/post_index.jsx
+++ frontend/src/components/postings/post_index.jsx
@@ -4,13 +4,23 @@ import PostIndexItem from './post_index_item';
 class PostIndex extends React.Component{
     constructor(props){
         super(props);
+        this
     }
 
     componentDidMount(){
         this.props.fetchPostings()
     }
 
+    check(){
+        if (this.props === null){
+            return(
+                <div>There are no posts available</div>
+            )
+        }
+    }
+
     render(){
+        {check}
         const { posts } = this.props;
         
         return(
--- frontend/src/components/postings/post_index_item.jsx
+++ frontend/src/components/postings/post_index_item.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import PostShowContainer from './post_show_container';
 
 class PostIndexItem extends React.Component{
     constructor(props){
         super(props);
         //modal goes here
+        this.sohwPost = this.showPost.bind(this);
+    }
+
+    showPost(post){
+        return(
+            <PostShowContainer post={post}/>
+        )
     }
 
 
@@ -11,10 +19,9 @@ class PostIndexItem extends React.Component{
         const { post } = this.props;
 
         return(
-            <div>
+            <div onClick={() => this.showPost(post)}>
                 <ul>
-                    <li>{post.title}</li> 
-                    {/* {need to add post.title to schema} */}
+                    <li>{post.job_title}</li> 
                     <li>{post.company}</li>
                     <li>{post.salary}</li>
                 </ul>
@@ -25,3 +32,5 @@ class PostIndexItem extends React.Component{
         )
     }
 }
+
+export default PostIndexItem;
