2a7671e3abed0a022123f224c1a96342af48cfb4
Sethbarrie <sethbarrie@gmail.com>
2020-10-27 11:13:52 -0700

    Added follow_users population to the users schema

--- models/User.js
+++ models/User.js
@@ -14,8 +14,13 @@ const UserSchema = new Schema({
     password: {
       type: String,
       required: true
-    }
-  }, {
+    },
+    followed_user: [{
+      type: Schema.Types.ObjectId,
+      ref: 'users'
+    }]
+  },
+   {
     timestamps: true
   })
 
