bd05ff3af7a70886996b74c17e5e8b578020a4eb
Mark Jang <mark.jang@yahoo.com>
2020-10-27 22:57:13 -0700

    Render session errors

--- frontend/src/components/session/session_form.jsx
+++ frontend/src/components/session/session_form.jsx
@@ -6,9 +6,12 @@ class SessionForm extends React.Component {
         this.state = this.props.user
         this.handleSubmit = this.handleSubmit.bind(this)
     }
+
+    componentWillUnmount(){
+        this.props.clearSessionErrors()
+    }
     handleSubmit(e){
         e.preventDefault()
-        debugger
         let user = this.state
         this.props.submitAction(user)
     }
@@ -17,15 +20,27 @@ class SessionForm extends React.Component {
             this.setState({[field]: e.currentTarget.value})
         }
     }
+
     render() {
-        debugger
         return <div className="session-form">
             <h3>{this.props.formType}</h3>
             <form onSubmit={this.handleSubmit}>
-                <input type="text" onChange={this.update("email")} value={this.state.email} placeholder="email" />
-                {this.props.formType === "Sign up" && <input type="text" placeholder="username" onChange={this.update("username")} value={this.state.username}/ >}
-                <input type="password" onChange={this.update("password")} value={this.state.password} placeholder="password" />
-                {this.props.formType === "Sign up" && <input type="password" placeholder="confirm password" onChange={this.update("password2")} value={this.state.password2}/ >}
+                <div>
+                    <input type="text" onChange={this.update("email")} value={this.state.email} placeholder="email" />
+                    {this.props.errors.email && <p>{this.props.errors.email}</p>}
+                </div>
+                <div>
+                    {this.props.formType === "Sign up" && <input type="text" placeholder="username" onChange={this.update("username")} value={this.state.username}/ >}
+                    {this.props.errors.username && <p>{this.props.errors.username}</p>}
+                </div>
+                <div>
+                    <input type="password" onChange={this.update("password")} value={this.state.password} placeholder="password" />
+                    {this.props.errors.password && <p>{this.props.errors.password}</p>}
+                </div>
+                <div>
+                    {this.props.formType === "Sign up" && <input type="password" placeholder="confirm password" onChange={this.update("password2")} value={this.state.password2}/ >}
+                    {this.props.errors.password2 && <p>{this.props.errors.password2}</p>}
+                </div>
                 <input type="submit" value={this.props.formType} />
             </form>
         </div>
