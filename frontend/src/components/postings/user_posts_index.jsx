f3a5c4efc43f103433397577144a71aa0b3b9b96
Mark Jang <mark.jang@yahoo.com>
2020-11-17 17:02:42 -0800

    Implement only show public jobs

--- frontend/src/components/postings/user_posts_index.jsx
+++ frontend/src/components/postings/user_posts_index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import UserPostsIndexItem from './user_posts_index_item';
 class UserPostsIndex extends React.Component {
     render() {
+        debugger
         {if (this.props.jobs.length < 1) {
             return <div>
                 No jobs
@@ -9,7 +10,7 @@ class UserPostsIndex extends React.Component {
         }}
         return <ul className="user-posts-index">
             {this.props.jobs.map(job => 
-                job === undefined 
+                job === undefined
                 ? null
                 : <UserPostsIndexItem 
                     key={job._id} 
