1925b794c3d4c29d77d0cbb3354061b4c4e704a0
Sethbarrie <sethbarrie@gmail.com>
2021-01-04 09:19:22 -0800

    Still attempting to fix router

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ const path = require('path');
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'));
-  app.get('/*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'public', 'index.html'));
   })
 }
 
