ad33ce3616a7853595c6415c050ffd5a9421d478
Sethbarrie <sethbarrie@gmail.com>
2021-01-06 12:51:13 -0800

    Trying to fix /get

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const path = require('path');
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'));
   app.get('/*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'public', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'frontend/public/index.html'));
   })
 }
 
