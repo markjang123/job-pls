73d85281a2af4e5bd5d64165115f8eb125c73a2d
Sethbarrie <sethbarrie@gmail.com>
2021-01-06 12:55:37 -0800

    Trying more stuff to fix canot get

--- app.js
+++ app.js
@@ -10,18 +10,20 @@ const bodyParser = require('body-parser');
 const passport = require('passport');
 const path = require('path');
 
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'));
-  app.get('/*', (req, res) => {
+  app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend/public/index.html'));
   })
 }
 
 
-mongoose
-  .connect(db, { useNewUrlParser: true })
-  .then(() => console.log("Connected to MongoDB successfully"))
-  .catch(err => console.log(err));
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
