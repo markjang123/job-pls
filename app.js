65a4db6c99724e3b0df6eed234f8bd0e5b92e6c3
Sethbarrie <sethbarrie@gmail.com>
2021-01-06 13:07:25 -0800

    Stiil trying to fix that route

--- app.js
+++ app.js
@@ -10,20 +10,18 @@ const bodyParser = require('body-parser');
 const passport = require('passport');
 const path = require('path');
 
-mongoose
-  .connect(db, { useNewUrlParser: true })
-  .then(() => console.log("Connected to MongoDB successfully"))
-  .catch(err => console.log(err));
-
-
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'));
+  app.use(express.static('frontend/public'));
   app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend/public/index.html'));
+    res.sendFile(path.resolve(__dirname, 'frontend', 'public', 'index.html'));
   })
 }
 
 
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
